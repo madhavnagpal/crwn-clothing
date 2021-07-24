58b3166ad4e2570b5d44942556cbd2b82adf9d91
madhavnagpal <mr.madhavnagpal@gmail.com>
2021-07-24 11:31:56 +0530

    created react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Homepage from "./components/Homepage/Homepage.component";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+      <p>Lets learn react</p>
+    </div>
+  );
+}
+
+export default App;
